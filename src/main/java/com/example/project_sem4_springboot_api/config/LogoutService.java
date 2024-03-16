49b35c99f8dcde65ca24ff253abe54987b3600d0
Nam Hoang <hnam17397@gmail.com>
2024-03-16 22:20:59 +0700

    1

---
+++ src/main/java/com/example/project_sem4_springboot_api/config/LogoutService.java
@@ -0,0 +1,29 @@
+package com.example.project_sem4_springboot_api.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+
+    @Override
+    public void logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+       // handle when user logout
+    }
+}
