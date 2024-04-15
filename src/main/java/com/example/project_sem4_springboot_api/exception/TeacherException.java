c613da2cfe845f104b8f6eec2d33c06f96f12266
Tulat2002 <126972147+Tulat2002@users.noreply.github.com>
2024-04-15 07:19:17 +0700

    ok

--- src/main/java/com/example/project_sem4_springboot_api/exception/TeacherException.java
+++
@@ -1,8 +0,0 @@
-package com.example.project_sem4_springboot_api.exception;
-
-public class TeacherException extends Exception {
-    public TeacherException(String message){
-        super(message);
-    }
-
-}
